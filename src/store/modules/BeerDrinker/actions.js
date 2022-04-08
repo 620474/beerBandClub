e9f19c15bbee6a081948ba3b457d05850b6520d8
Maksim Sychev <maksim_sychev@epam.com>
2022-04-08 19:24:57 +0300

    refactor store

--- src/store/modules/BeerDrinker/actions.js
+++ src/store/modules/BeerDrinker/actions.js
@@ -1,28 +1,26 @@
-
 import mutationTypes from './mutationTypes';
 
 const actions = {
-    getBeer({ commit }) {
-    try {
-      commit(mutationTypes.SET_BEER_LOADING);
-        fetch("https://random-data-api.com/api/beer/random_beer")
-          .then(response => {return response.json()})
-          .then(info=>commit(mutationTypes.SET_BEER_SUCCESS, info))
-    } catch (error) {
-      commit(mutationTypes.SET_BEER_FAIL, error.code);
-    }
-  },
+    getBeer({commit}) {
 
-   getDrinker({ commit }) {
-    try {
-      commit(mutationTypes.SET_DRINKER_LOADING);
-       fetch("https://random-data-api.com/api/users/random_user ")
-          .then(response => {return response.json()})
-          .then(info=>commit(mutationTypes.SET_DRINKER_SUCCESS, info))
-    } catch (error) {
-      commit(mutationTypes.SET_DRINKER_FAIL, error.code);
+        commit(mutationTypes.SET_BEER_LOADING);
+        fetch("https://random-data-api.com/api/beer/random_beer")
+            .then(response => {
+                return response.json()
+            })
+            .then(info => commit(mutationTypes.SET_BEER_SUCCESS, info))
+            .catch(error => commit(mutationTypes.SET_BEER_FAIL, error.code))
+    },
+
+    getDrinker({commit}) {
+            commit(mutationTypes.SET_DRINKER_LOADING);
+            fetch("https://random-data-api.com/api/users/random_user ")
+                .then(response => {
+                    return response.json()
+                })
+                .then(info => commit(mutationTypes.SET_DRINKER_SUCCESS, info))
+                .catch(error => commit(mutationTypes.SET_BEER_FAIL, error.code))
     }
-  },
 };
 
 export default actions;
